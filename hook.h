2010c75da5c1357e12c75da554364094616b7de3
Muntadhar Mohammed Rasheed <muntadhar.mohammed_rasheed.iku@student.atu.edu.iq>
2026-07-22 16:08:52 +0300

    Refactor hook.h to simplify hook management functions

--- hook.h
+++ hook.h
@@ -1,28 +1,16 @@
 #ifndef HOOK_H
 #define HOOK_H
-
 #include <stdbool.h>
-#include <stdint.h>
-#include <mach/mach.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// تعريف عدد الـ Hooks
-#ifndef MAX_HW_HOOKS
-#define MAX_HW_HOOKS 6
-#endif
-
-struct hook_info {
-    uintptr_t target_address;
-    uintptr_t replacement_address;
-};
-
-bool hook_install(void *targets[], void *replacements[], int count);
-
+bool hook_init(void);
+bool hook_install(void *target, void *replacement);
+bool hook_uninstall(void *target);
+bool hook_pause_all(void);
+bool hook_resume_all(void);
+void hook_deinit(void);
 #ifdef __cplusplus
 }
 #endif
-
-#endif // HOOK_H
+#endif
